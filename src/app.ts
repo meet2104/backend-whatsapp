1a425a1b3259091ad549ed9c744b81cebf2a6ce5
Meet <goswamimeet380@gmail.com>
2026-03-26 16:02:17 +0530

    Added changes

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 ========================= */
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["https://frontend-whatsapp-five.vercel.app/login", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
